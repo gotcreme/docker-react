56e3ab2efae5fd1ad4ebaaa03405489520462c7b
Volodymyr Dykun <vdy@e-Boks.dk>
2019-10-10 14:09:18 +0300

    text change to test ci/cd pipeline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hey you
+          Experimental change to file.
         </p>
         <a
           className="App-link"
